a7672f4712108df672291a8d8453c26c8e54f1ec
Matrix30 <Matrixboy30@gmail.com>
2024-02-02 06:34:45 +0100

    finish /stats route

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,6 +2,7 @@
 """ implements various api routes"""
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
 
 
 @app_views.route("/status")
@@ -11,3 +12,17 @@ def status():
         "status": "OK"
     }
     return jsonify(api_status)
+
+
+@app_views.route("/stats")
+def stats():
+    """ method to give the statistics of all stored objects"""
+    # mapping of classes to class name
+    classes = {"amenities": "Amenity", "cities": "City", "places": "Place",
+               "reviews": "Review", "states": "State", "users": "User"}
+    statistics = {}
+
+    for key, val in classes.items():
+        statistics[key] = storage.count(val)
+
+    return jsonify(statistics)
