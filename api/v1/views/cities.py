47b562fa1702b6748d6a83554bc12def8f4d439d
Matrix30 <Matrixboy30@gmail.com>
2024-02-05 11:47:34 +0100

    fix cities22

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -41,26 +41,27 @@ def delete_city(city_id):
     abort(404)
 
 
-@app_views.route('/states/<state_id>/cities', methods=['POST'],
+@app_views.route("/states/<state_id>/cities", methods=['POST'],
                  strict_slashes=False)
 def create_city(state_id):
-    """Creates a City"""
-    state = storage.get(State, state_id)
-    if state is None:
+    """method to create new city under a given state"""
+    valid_state = False
+    for state in storage.all(State).values():
+        if state.id == state_id:
+            valid_state = True
+    if not valid_state:
         abort(404)
 
-    req_json = request.get_json()
-    if not req_json:
-        abort(400, 'Not a JSON')
-    if 'name' not in req_json:
-        abort(400, 'Missing name')
+    request_data = request.get_json(silent=True)
+    if request_data is None:
+        abort(400, "Not a JSON")
+    if 'name' not in request_data:
+        abort(400, "Missing name")
 
-    req_json['state_id'] = state_id
-    new_city = City(**req_json)
-    storage.new(new_city)
-    storage.save()
-
-    return jsonify(new_city.to_dict()), 201
+    request_data["state_id"] = state_id
+    new_city = City(**request_data)
+    new_city.save()
+    return make_response(jsonify(new_city.to_dict()), 201)
 
 
 @app_views.route("/cities/<city_id>", methods=['PUT'], strict_slashes=False)
